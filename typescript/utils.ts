b8747eb309157f9c037674a0c90b715e3bd80cb2
tristan <heytristaann@gmail.com>
2023-08-07 09:58:24 -0400

    cleanup

---
+++ typescript/utils.ts
@@ -0,0 +1,29 @@
+/**
+ * Check if the value is a number
+ *
+ * @param value The value to check
+ * @returns True if the value is a number, false otherwise
+ * @example
+ * isNumber("5");
+ * // => true
+ * isNumber("hello");
+ * // => false
+ */
+export const isNumber = (value: string): boolean => {
+  return value.match(/[0-9]/i) ? true : false;
+};
+
+/**
+ * Check if the value is a string
+ *
+ * @param value The value to check
+ * @returns True if the value is a string, false otherwise
+ * @example
+ * isString("hello");
+ * // => true
+ * isString("5");
+ * // => false
+ */
+export const isString = (value: string): boolean => {
+  return value.match(/[a-z]/i) ? true : false;
+};
